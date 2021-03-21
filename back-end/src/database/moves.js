f24fda0677d6583bd6133d4710127ecacc3a0633
Marcoshsc <marcoshscunha@hotmail.com>
2021-03-21 16:17:26 -0300

    adjusted game time

--- back-end/src/database/moves.js
+++ back-end/src/database/moves.js
@@ -7,15 +7,28 @@ async function createMove(game, from, to) {
   const gameTotalTime = Number.parseInt(timeInfo[0]) * 60
   const increment = Number.parseInt(timeInfo[1])
 
-  const t1 = 'select coalesce(max(sequencial), 0) as s from lance where id_partida=$1'
-  const p1 = [game.id]
-  const totalMoves = await client.query(t1, p1)
-  const date1 = new Date(game.data_criacao)
+  const lastMoveQuery = 'select momento_lance, tempo from lance where id_partida=$1 and ' +
+  'sequencial in((select max(sequencial) from lance where id_partida=$1), (select max(sequencial - 1) from lance where id_partida=$1)) order by sequencial'
+  const lastMoveParams = [game.id]
+  const lastMoveR = await client.query(lastMoveQuery, lastMoveParams)
+  const lastMove = lastMoveR.rows[1]
+  const lastOwnMove = lastMoveR.rows[0]
+
+  let remaining
   const date2 = new Date()
-  const remaining = Math.floor(((gameTotalTime - ((date2 - date1) / 1000))) + (Math.floor(totalMoves.rows[0].s / 2) * increment) + increment)
+  if(lastMove) {
+    const date1 = new Date(lastMove.momento_lance)
+    remaining = Math.floor((lastOwnMove.tempo - ((date2 - date1) / 1000)) + increment)
+    console.log(`Tinha ${lastMove.tempo}, gastou: `)
+    console.log(((date2 - date1) / 1000))
+    console.log(`Agora tem: ${remaining}`)
+  }
+  else {
+    remaining = gameTotalTime
+  }
 
   const text = 'insert into lance (id_partida, sequencial, tempo, de, para, momento_lance) values ' + 
-  '($1, coalesce((select max(sequencial) + 1 from lance where id_partida=$1), 1), $2, $3, $4) returning tempo, de, para, sequencial'
+  '($1, coalesce((select max(sequencial) + 1 from lance where id_partida=$1), 1), $2, $3, $4, $5) returning tempo, de, para, sequencial, momento_lance'
   const params = [game.id, remaining, from, to, date2]
   const result = await client.query(text, params)
   await client.end()
